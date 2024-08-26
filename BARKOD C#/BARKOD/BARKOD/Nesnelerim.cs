c599d59902cb655adc00914298d4e3a274f51950
Sinem18 <sinemincekara@ogr.bandirma.edu.tr>
2024-08-26 14:17:05 +0300

    Özel araç Ekleme.

--- BARKOD C#/BARKOD/BARKOD/Nesnelerim.cs
+++ BARKOD C#/BARKOD/BARKOD/Nesnelerim.cs
@@ -24,7 +24,7 @@ namespace BARKOD
 
         }
     }
-    public class bStandart: Button
+    public class bStandart : Button
     {
         public bStandart()
         {
@@ -39,7 +39,7 @@ namespace BARKOD
             this.TabIndex = 5;
             this.Text = "NAKİT\r\n(F1)";
             this.UseVisualStyleBackColor = false;
-            
+
         }
     }
 
@@ -49,7 +49,7 @@ namespace BARKOD
         {
             this.Size = new System.Drawing.Size(250, 26);
             this.BackColor = System.Drawing.Color.White;
-            this.Font =  new System.Drawing.Font("Microsoft Sans Serif",12F);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
 
         }
     }
@@ -60,8 +60,54 @@ namespace BARKOD
         {
             this.Size = new System.Drawing.Size(115, 26);
             this.BackColor = System.Drawing.Color.White;
-            this.Font =  new System.Drawing.Font("Microsoft Sans Serif",12F);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Name = "tNumeric";
+            this.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.Click += TNumeric_Click;
+            this.KeyPress += TNumeric_KeyPress;
+
+        }
+
+        private void TNumeric_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //throw new NotImplementedException();
+            if (char.IsDigit(e.KeyChar) == false && e.KeyChar != (char)08)
+            {
+                e.Handled = true;
+
+
+            }
+        }
+
+        private void TNumeric_Click(object sender, EventArgs e)
+        {
+            // throw new NotImplementedException();
+            this.SelectAll();
         }
     }
+    public class gridOzel : DataGridView
+    {
+        public gridOzel()
+        {
+            this.AllowUserToAddRows = false;
+            this.BackgroundColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dock = System.Windows.Forms.DockStyle.None;
+            this.EnableHeadersVisualStyles = false;
+            this.Location = new System.Drawing.Point(3, 132);
+            this.Name = "dgrid";
+            this.RowHeadersVisible = false;
+            this.RowHeadersWidth = 51;
+            this.RowTemplate.DefaultCellStyle.Padding = new System.Windows.Forms.Padding(3);
+            this.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.Silver;
+            this.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.RowTemplate.Height = 30;
+            this.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.Size = new System.Drawing.Size(577, 465);
+            this.TabIndex = 0;
 
+        }
+
+    }
 }
